cd98f919fa1fcf4d49f2698a0acfb1b5a55db340
alyaanafea <alyaanafea@gmail.com>
2024-03-16 22:21:53 +0200

    initial login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import WhiteLogo from "../component/logo/WhiteLogo";
 import "./loginStyle.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function Login() {
   return (
@@ -25,19 +27,59 @@ function Login() {
             <p className="font-medium text-base tracking-wider">
               If you are new user please sign up to our world
             </p>
-            <Link to={"/register"}
+            <Link
+              to={"/register"}
               className="btn btn-wide bg-transparent border-white	text-white btnhover"
-              
             >
               Sign up now for free...
             </Link>
           </div>
         </div>
-        <div className="bg-gray-300 p-4">
-          <form>
 
-
-
+        <div className="bg-white flex flex-col justify-center items-center">
+          <form className="border rounded-lg shadow-md  flex flex-col text-black gap-12 justify-center items-center py-16 px-24">
+            <div className="flex flex-col gap-4">
+              <h2 className="text-2xl font-bold">Back to your world</h2>
+              <p>Choose one of the option to go</p>
+            </div>
+            <div className="flex flex-col gap-6">
+              <input
+                type="email"
+                placeholder="example@example.com"
+                className="input input-bordered w-full "
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                className="input input-bordered w-full "
+              />
+              <div className="flex justify-between lg:gap-20 md:gap-5 ">
+                <div>
+                  <input
+                    type="checkbox"
+                    className="checkbox mr-1 checkbox-xs"
+                  />
+                  <label htmlFor="#rememberme">Remember Me</label>
+                </div>
+                <p>Forgot password?</p>
+              </div>
+            </div>
+            <div className="flex justify-between w-full">
+              <div>
+                <FontAwesomeIcon icon={faGithub} />
+              </div>
+              <div>
+                <FontAwesomeIcon icon={faGithub} />
+              </div>
+              <div>
+                <FontAwesomeIcon icon={faGithub} />
+              </div>
+             
+            </div>
+            <div>
+              <button></button>
+              <p></p>
+            </div>
           </form>
         </div>
       </div>
