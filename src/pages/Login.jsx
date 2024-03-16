fe904e755facdbd273a4f60c6c9e52211b91d6ef
Ahmed Yahia El-asmer <57156526+AhmedYahiaElasmer@users.noreply.github.com>
2024-03-16 22:09:26 +0200

    Login (#4)
    * login initial ui
    
    * register initial ui
    
    ---------
    
    Co-authored-by: alyaanafea <alyaanafea@gmail.com>

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,48 @@
+import { Link } from "react-router-dom";
+import WhiteLogo from "../component/logo/WhiteLogo";
+import "./loginStyle.css";
+
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="">
+      <div className="grid grid-cols-2 h-screen bg-black ">
+        <div className="min-h-full text-white flex flex-col justify-between items-center lg:pt-14 lg:pb-24 md:py-14 bgc">
+          <div className="flex flex-col lg:gap-7  justify-center items-center">
+            <div>
+              {" "}
+              <WhiteLogo />
+            </div>
+            <div className="flex flex-col justify-center items-center gap-1 ">
+              <h1 className="text-5xl tracking-[.15em] font-semibold">
+                TYPING....
+              </h1>
+              <p className="font-thin text-base tracking-wider">
+                Connect to the world in few touches
+              </p>
+            </div>
+          </div>
+          <div className=" pb-16 flex flex-col justify-center items-center gap-5">
+            <p className="font-medium text-base tracking-wider">
+              If you are new user please sign up to our world
+            </p>
+            <Link to={"/register"}
+              className="btn btn-wide bg-transparent border-white	text-white btnhover"
+              
+            >
+              Sign up now for free...
+            </Link>
+          </div>
+        </div>
+        <div className="bg-gray-300 p-4">
+          <form>
+
+
+
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
