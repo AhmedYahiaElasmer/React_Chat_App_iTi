2a3ca9ab42665f0d3fabe7864c1c62ad108d5544
AhmedYahiaElasmer <ahmedyahiarabeaa2001@gmail.com>
2024-03-16 23:47:43 +0200

    create register component

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,5 @@
-import { Link } from "react-router-dom";
-import BlackLogo from "../component/logo/BlackLogo";
 import "./loginStyle.css";
+import RegisterLogo from "../component/register/RegisterLogo";
 
 function Register() {
   return (
@@ -9,29 +8,7 @@ function Register() {
         <div className="p-4 bg-black bgc">
           <form></form>
         </div>
-        <div className="min-h-full text-black flex flex-col justify-between items-center lg:pt-14 lg:pb-24 md:py-14 ">
-          <div className="flex flex-col lg:gap-7  justify-center items-center">
-            <div>
-              {" "}
-              <BlackLogo />
-            </div>
-            <div className="flex flex-col justify-center items-center gap-1 ">
-              <h1 className="text-5xl tracking-[.15em] font-semibold">
-                TYPING....
-              </h1>
-              <p className="font-normal text-base tracking-wider">
-                Connect to the world in few touches
-              </p>
-            </div>
-          </div>
-          <div className=" pb-16 flex flex-col justify-center items-center gap-5">
-            <p className="font-medium text-base tracking-wider">
-            If you are already member login now...
-            </p>
-            <Link to={"/"} className="btn btn-wide text-black bg-transparent border-black	 btnhover hover:text-white" >
-Login now...  </Link>
-          </div>
-        </div>
+        <RegisterLogo />
       </div>
     </div>
   );
