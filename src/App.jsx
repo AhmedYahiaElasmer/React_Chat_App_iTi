f0bac9f4f5f32c8f4d1ce6ecce132f83bb0fcad5
AhmedYahiaElasmer <ahmedyahiarabeaa2001@gmail.com>
2024-03-17 04:34:08 +0200

    firebase avtare in register

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       </Routes> */}
 
       <Login />
-      {/* <Register/> */}
+      <Register />
     </>
   );
 }
--- src/component/register/RegisterForm.jsx
+++ src/component/register/RegisterForm.jsx
@@ -1,6 +1,23 @@
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
+
+import { useState } from "react";
+import { UplaodFile } from "../utils/uploadimage";
+
 function RegisterForm() {
+  const [file, setFile] = useState();
+
+  const uploadAvatarHandler = async () => {
+    try {
+      if (file) {
+        const urlFile = await UplaodFile("avatar", file);
+        console.log(urlFile);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const {
     register,
     handleSubmit,
@@ -8,112 +25,127 @@ function RegisterForm() {
   } = useForm();
   const onSubmit = (data) => console.log(data);
   return (
-    <div className="p-4 bg-black bgc">
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="  flex flex-col text-white gap-12 justify-center items-center py-16 px-24"
-      >
-        <div className="flex flex-col gap-4 ">
-          <h1 className="text-2xl font-extrabold">Create new account</h1>
-        </div>
-        <div className="flex flex-col gap-6">
-          <div className="flex">
-            <div className="flex flex-col ">
-              <label htmlFor="fristName" className="w-auto">
-                Frist Name
-              </label>
+    <>
+      <div className=" flex flex-col gap-1 items-center">
+        <label htmlFor="avatar">Upload File</label>
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type="file"
+          name="avatar"
+          id=""
+        />
+        <button onClick={uploadAvatarHandler} className="btn btn-outline">
+          Upload
+        </button>
+      </div>
 
-              <input
-                {...register("fristName", { required: true })}
-                id="fristName"
-                type="text"
-                placeholder="fristName"
-                className="input input-bordered  text-black w-auto "
-              />
-              {errors.fristName && (
-                <span className="text-red-600">This field is required</span>
-              )}
-            </div>
-            <div className="flex flex-col ">
-              <label htmlFor="lastName" className="w-auto ">
-                Last Name
-              </label>
-              <input
-                {...register("lastName", { required: true })}
-                id="lastName"
-                type="text"
-                placeholder="lastName"
-                className="input bg-transparent input-bordered text-black  w-auto  "
-              />
-              {errors.lastName && (
-                <span className="text-red-600">This field is required</span>
-              )}
-            </div>
+      <div className="p-4 bg-black bgc">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="  flex flex-col text-white gap-12 justify-center items-center py-16 px-24"
+        >
+          <div className="flex flex-col gap-4 ">
+            <h1 className="text-2xl font-extrabold">Create new account</h1>
           </div>
+          <div className="flex flex-col gap-6">
+            <div className="flex">
+              <div className="flex flex-col ">
+                <label htmlFor="fristName" className="w-auto">
+                  Frist Name
+                </label>
 
-          <label htmlFor="email" className="w-auto">
-            E-mail
-          </label>
-          <input
-            {...register("email", { required: true })}
-            id="email"
-            type="email"
-            placeholder="example@example.com"
-            className="input input-bordered text-black  w-auto "
-          />
-          {errors.email && (
-            <span className="text-red-600">This field is required</span>
-          )}
+                <input
+                  {...register("fristName", { required: true })}
+                  id="fristName"
+                  type="text"
+                  placeholder="fristName"
+                  className="input input-bordered  text-black w-auto "
+                />
+                {errors.fristName && (
+                  <span className="text-red-600">This field is required</span>
+                )}
+              </div>
+              <div className="flex flex-col ">
+                <label htmlFor="lastName" className="w-auto ">
+                  Last Name
+                </label>
+                <input
+                  {...register("lastName", { required: true })}
+                  id="lastName"
+                  type="text"
+                  placeholder="lastName"
+                  className="input bg-transparent input-bordered text-black  w-auto  "
+                />
+                {errors.lastName && (
+                  <span className="text-red-600">This field is required</span>
+                )}
+              </div>
+            </div>
 
-          <label htmlFor="password" className="w-auto">
-            Password
-          </label>
-          <input
-            {...register("password", { required: true })}
-            id="password"
-            type="password"
-            placeholder="enter your password"
-            className="input input-bordered text-black  w-auto "
-          />
-          {errors.password && (
-            <span className="text-red-600">This field is required</span>
-          )}
-          <label htmlFor="Re-password" className="w-auto">
-            Re-Password
-          </label>
-          <input
-            {...register("Repassword", { required: true })}
-            id="Re-password"
-            type="password"
-            placeholder="enter your Re-password"
-            className="input input-bordered text-black  w-auto "
-          />
-          {errors.Repassword && (
-            <span className="text-red-600">This field is required</span>
-          )}
-        </div>
-        <div>
-          <button
-            type="submit"
-            className="btn  btn-wide text-white bg-transparent border-white	 btnhover"
-          >
-            Sign Up
-          </button>
-          <p></p>
-        </div>
-        <div className="flex justify-between lg:gap-20 md:gap-5 ">
-          <p>
-            Already a member?
-            <Link
-              to=""
-              className="link link-success link-hover  link-linkColor"
+            <label htmlFor="email" className="w-auto">
+              E-mail
+            </label>
+            <input
+              {...register("email", { required: true })}
+              id="email"
+              type="email"
+              placeholder="example@example.com"
+              className="input input-bordered text-black  w-auto "
+            />
+            {errors.email && (
+              <span className="text-red-600">This field is required</span>
+            )}
+
+            <label htmlFor="password" className="w-auto">
+              Password
+            </label>
+            <input
+              {...register("password", { required: true })}
+              id="password"
+              type="password"
+              placeholder="enter your password"
+              className="input input-bordered text-black  w-auto "
+            />
+            {errors.password && (
+              <span className="text-red-600">This field is required</span>
+            )}
+            <label htmlFor="Re-password" className="w-auto">
+              Re-Password
+            </label>
+            <input
+              {...register("Repassword", { required: true })}
+              id="Re-password"
+              type="password"
+              placeholder="enter your Re-password"
+              className="input input-bordered text-black  w-auto "
+            />
+            {errors.Repassword && (
+              <span className="text-red-600">This field is required</span>
+            )}
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="btn  btn-wide text-white bg-transparent border-white	 btnhover"
             >
-              Sign in
-            </Link>
-          </p>
-        </div>
-      </form>
-    </div>
+              Sign Up
+            </button>
+            <p></p>
+          </div>
+          <div className="flex justify-between lg:gap-20 md:gap-5 ">
+            <p>
+              Already a member?
+              <Link
+                to=""
+                className="link link-success link-hover  link-linkColor"
+              >
+                Sign in
+              </Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
 
