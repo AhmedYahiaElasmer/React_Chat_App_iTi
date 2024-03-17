bb8252f17e007486dbc52108c436c9f5a922fbc2
Ahmed Yahia El-asmer <57156526+AhmedYahiaElasmer@users.noreply.github.com>
2024-03-17 16:55:23 +0200

    Register (#14)
    * create register component
    
    * register created
    
    * final form after avtar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mytheme: {
+          white: "#FFFFFF",
+
+          grad_Color: "bg-gradient-to-r from-blue-700 to-purple-800",
+        },
+      },
+      backgroundImage: {
+        "grad-color": "linear-gradient(90deg, #041AD8 0%, #B012AA 78.84%)",
+      },
+    },
   },
   daisyui: {
     themes: [
@@ -15,6 +26,11 @@ export default {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light"],
+    themes: ["mytheme", "light"],
+    extend: {
+      colors: {
+        "grad-color": "var(--gradient-color)", // Define as a CSS variable
+      },
+    },
   },
 };
