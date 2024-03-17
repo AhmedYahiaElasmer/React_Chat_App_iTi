a06a0c5d3a1d8102d828b29eb127bcfe0a53c591
AhmedYahiaElasmer <ahmedyahiarabeaa2001@gmail.com>
2024-03-17 03:27:47 +0200

    register created

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,12 @@
 import "./loginStyle.css";
 import RegisterLogo from "../component/register/RegisterLogo";
+import RegisterForm from "../component/register/RegisterForm";
 
 function Register() {
   return (
     <div className="">
       <div className="grid grid-cols-2 h-screen  ">
-        <div className="p-4 bg-black bgc">
-          <form></form>
-        </div>
+        <RegisterForm />
         <RegisterLogo />
       </div>
     </div>
