1fc682b35b667657672d82170fede020ab7156c7
Ahmed Yahia El-asmer <57156526+AhmedYahiaElasmer@users.noreply.github.com>
2024-03-17 03:26:49 +0200

    add firebase config and upload function (#10)
    Co-authored-by: Abdulrhaman-Mohamed <abdorrr2015@gmail.com>

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,35 @@
+
+//import { useState } from "react";
+//import { UplaodFile } from "../utils/uploadimage";
+
+//function Login() {
+
+  // const [file,setFile] = useState();
+
+  // const uploadAvatarHandler=async()=>{
+  //   try {
+  //     if(file){
+  //       const urlFile = await UplaodFile("avatar",file);
+  //       console.log(urlFile);
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+
+  // return(
+  //   <>
+  //   <div className=" flex flex-col gap-1 items-center">
+  //   <label htmlFor="avatar">Upload File</label>
+  //   <input onChange={(e)=> setFile(e.target.files[0])} type="file" name="avatar"  id="" />
+  //   <button onClick={uploadAvatarHandler} className="btn btn-outline">Upload</button>
+  //   </div>
+
+  //   </>
+  // )
+ // return <div>Login</div>;
+
 import "./loginStyle.css";
 
 
@@ -20,6 +52,7 @@ function Login() {
       </div>
     </div>
   );
+
 }
 
 export default Login;
