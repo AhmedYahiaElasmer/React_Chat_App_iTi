1ceca298bada91efe5f4d5e8ebfadaeb61fdf546
مصطفي نجم <amelfareed@gmail.com>
2024-03-21 01:44:27 +0200

    merge changes

--- src/component/home/MsgsContainer.jsx
+++ src/component/home/MsgsContainer.jsx
@@ -3,13 +3,11 @@ import ChatNavBar from "../messages/ChatNavBar";
 import ChatInput from "./ChatInput";
 import { Outlet } from "react-router-dom";
 import './Nav.css'
-import { useAuthContext } from "../../context/AuthContext";
-import GrayLogo from "../logo/GrayLogo";
 const MsgsContainer = () => {
   return (
     <div className="background h-screen text-white relative">
       {/* navbar */}
-      <ChatNavBar className="nav" />
+      <ChatNavBar className='nav'/>
       {/* msg-container */}
       <div className="px-4  flex-1 flex flex-col overflow  w-full ">
         <Outlet />
@@ -17,10 +15,6 @@ const MsgsContainer = () => {
           {/* <ChatBar /> */}
          
         
-
-        <div className="absolute bottom-0 -left-[-23.5%]   w-3/4 ">
-          <ChatBar />
-        </div>
       </div>
       <ChatInput className='position'/>
     </div>
